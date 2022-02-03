04a8d0d4f3197467cc881374569dcfcf9746d071
ToniMaciel <tonialvesmaciel@gmail.com>
2022-02-03 17:00:34 -0300

    Change wrong dependecy to cause the failure

--- src/test/java/org/PersonTest.java
+++ src/test/java/org/PersonTest.java
@@ -1,6 +1,6 @@
 package org;
 
-import org.Person;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,6 +9,19 @@ public class PersonTest {
   @Test
   public void expectPassedTest(){
     Person person = new Person("Ana", 2000, "Student");
+    Assert.assertEquals("Ana", person.getName());
+    Assert.assertEquals(2000, person.getBirthYear());
+    Assert.assertFalse(person.isTeenager());
+    Assert.assertNotNull(person.getOccupation());
+  }
+
+  @Test
+  public void brokenEqualsBuilderTest(){
+    Person person = new Person("Ana", 2000, "Student");
+    Person person2 = new Person("Vitoria", 2000, "Student");
+    EqualsBuilder equalsBuilder = new EqualsBuilder();
+    //Wrong version of commons-lang dependency to cause a compilation failure when the tool try to use test-compile
+    equalsBuilder.setTestRecursive(true);
     Assert.assertEquals("Ana", person.getName());
     Assert.assertEquals(2000, person.getBirthYear());
     Assert.assertFalse(person.isTeenager());
