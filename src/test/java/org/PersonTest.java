70fcc5af960e08ac057dfc5f3990225fafa9fd7d
ToniMaciel <tonialvesmaciel@gmail.com>
2022-02-15 16:10:13 -0300

    Mockito broken test

--- src/test/java/org/PersonTest.java
+++ src/test/java/org/PersonTest.java
@@ -1,8 +1,10 @@
 package org;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
+import static org.mockito.Mockito.mockConstruction;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.MockedConstruction;
 
 public class PersonTest {
 
@@ -19,9 +21,10 @@ public class PersonTest {
   public void brokenEqualsBuilderTest(){
     Person person = new Person("Ana", 2000, "Student");
     Person person2 = new Person("Vitoria", 2000, "Student");
-    EqualsBuilder equalsBuilder = new EqualsBuilder();
-    //Wrong version of commons-lang dependency to cause a compilation failure when the tool try to use test-compile
-    equalsBuilder.setTestRecursive(true);
+    //Wrong version of mockito dependency to cause a compilation failure when the tool try to use test-compile
+    try (MockedConstruction mocked = mockConstruction(Person.class)){
+      Person person3 = new Person("Jooj", 2000, "Student");
+    }
     Assert.assertEquals("Ana", person.getName());
     Assert.assertEquals(2000, person.getBirthYear());
     Assert.assertFalse(person.isTeenager());
